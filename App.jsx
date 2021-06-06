0dab363ff9e6d1a6eb2dc303aadf6884905e82d5
nikhil1807 <58559167+nikhil1807@users.noreply.github.com>
2021-06-06 17:10:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Note from "./Components/Note";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript & ReactJs Bootcamp</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad debitis
+        odio adipisci laborum porro esse et fuga natus rerum nam?
+      </p>
+    </div>
+  );
+}
+
+export default Note;
